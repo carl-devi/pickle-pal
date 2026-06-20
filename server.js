ed729cb1c3ee6c8046ebd67a5b2297f2a584887a
Carl Eduardo <noparking280@gmail.com>
2026-06-20 11:51:51 +0800

    Fixed Connection Problem

--- server.js
+++ server.js
@@ -45,11 +45,16 @@ db.run(`CREATE TABLE IF NOT EXISTS bookings (
 // --- NEW: Email Configuration (Nodemailer) ---
 // ===================================================================
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 const MANAGER_EMAIL = process.env.MANAGER_EMAIL;
